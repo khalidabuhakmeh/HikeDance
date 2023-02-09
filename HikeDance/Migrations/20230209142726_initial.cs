c8d7414ae1cc9ee7edc16081a6db7e4ec692d95c
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-02-09 09:29:40 -0500

    initial commit

---
+++ HikeDance/Migrations/20230209142726_initial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HikeDance.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    DayOfWeek = table.Column<byte>(type: "TINYINT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dances", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dances",
+                columns: new[] { "Id", "DayOfWeek", "Name" },
+                values: new object[] { 1, (byte)6, "Ho Down Spectacular!" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dances");
+        }
+    }
+}
---
+++ HikeDance/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+var db = new Database();
+db.Database.Migrate();
+
+var now = DateTime.Now;
+var dance = db.Dances.First(d => d.DayOfWeek == DayOfWeek.Saturday);
+
+var next = Next(now, dance.DayOfWeek);
+
+Console.WriteLine($"The next \"{dance.Name}\" is on {next:d}");
+
+static DateTime Next(DateTime from, DayOfWeek dayOfTheWeek)
+{
+    var date = from.Date.AddDays(1);
+    var days = ((int)dayOfTheWeek - (int)date.DayOfWeek + 7) % 7;
+    return date.AddDays(days);
+}
+
+public class Database : DbContext
+{
+    public DbSet<Dance> Dances => Set<Dance>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=database.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Dance>()
+            .Property(p => p.DayOfWeek)
+            .HasColumnType("TINYINT")
+            .HasConversion(
+                v => (byte)v,
+                v => (DayOfWeek)v    
+            );
+            
+        modelBuilder
+            .Entity<Dance>()
+            .HasData(new Dance
+            {
+                Id = 1,
+                Name = "Ho Down Spectacular!",
+                DayOfWeek = DayOfWeek.Saturday
+            });
+    }
+}
+
+public class Dance
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public DayOfWeek DayOfWeek { get; set; } = DayOfWeek.Sunday;
+}
